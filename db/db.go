503a48cc2e7db6462dd8f0a22c56c5180548017a
Xander Flood <xander.t.flood@gmail.com>
2020-08-25 19:12:32 -0400

    better error handling and fixing the migration init

--- db/db.go
+++ db/db.go
@@ -5,9 +5,6 @@ import (
 	"database/sql"
 	"fmt"
 	"time"
-
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 const (
--- main.go
+++ main.go
@@ -10,11 +10,14 @@ import (
 
 	migrate "github.com/golang-migrate/migrate/v4"
 	flags "github.com/jessevdk/go-flags"
-	_ "github.com/lib/pq" //provides the postgres driver for database/sql
 	"github.com/sirupsen/logrus"
 
 	"github.com/smartatransit/feedback/api"
 	"github.com/smartatransit/feedback/db"
+
+	"github.com/golang-migrate/migrate/v4/database/postgres" //provides the postgres driver for migrations
+	_ "github.com/golang-migrate/migrate/v4/source/file"     //provides the driver for filesystem-backed migrations
+	_ "github.com/lib/pq"                                    //provides the postgres driver for database/sql
 )
 
 var opts struct {
@@ -36,13 +39,22 @@ func main() {
 
 	database, err := sql.Open("postgres", opts.PostgresURL)
 	if err != nil {
-		logger.Error(err.Error())
+		logger.Errorf("failed to open postgres connection: %s", err.Error())
 		log.Fatal()
 	}
 
-	migrator, err := migrate.New("file", "file://"+opts.MigrationsPath)
+	mgdb, err := postgres.WithInstance(database, &postgres.Config{})
 	if err != nil {
-		logger.Error(err.Error())
+		logger.Errorf("failed to wrap postgres connection for migrations: %s", err.Error())
+		log.Fatal()
+	}
+
+	migrator, err := migrate.NewWithDatabaseInstance(
+		"file://"+opts.MigrationsPath,
+		"postgres", mgdb,
+	)
+	if err != nil {
+		logger.Errorf("failed to open migration client: %s", err.Error())
 		log.Fatal()
 	}
 
@@ -52,7 +64,7 @@ func main() {
 
 	err = dbClient.Migrate(context.Background())
 	if err != nil && !strings.Contains(err.Error(), "no change") {
-		logger.Error(err.Error())
+		logger.Errorf("failed to execute pending migrations: %s", err.Error())
 		log.Fatal()
 	}
 
